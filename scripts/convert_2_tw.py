a837113065812631e84424611aac35033b300f30
Palm <palm@gmail.com>
2022-01-31 16:28:48 +0800

    添加了繁体数据库脚本

--- scripts/convert_2_tw.py
+++ scripts/convert_2_tw.py
@@ -1,5 +1,5 @@
 from zhconv import convert_for_mw
 
-text = "日语求职面试"
+text = "一个适合日语初学者的博客"
 txt = convert_for_mw(text, 'zh-tw')
 print(txt)
--- scripts/new_2_hant.py
+++ scripts/new_2_hant.py
@@ -1,11 +1,6 @@
-# -*- coding:UTF-8 -*-
-# @Time : 2022/1/30 15:08
-# @Author : Palm
-# @Remark :
 from zhconv import convert_for_mw
 from izone.database_config import data_config
 import pymysql
-
 
 conn = pymysql.Connect(
     host=data_config['DATAHOST'],
@@ -18,30 +13,132 @@ conn = pymysql.Connect(
 
 cur = conn.cursor()
 
-sql = "select * from blog_category"
-sql_hant = "select * from blog_categoryhant"
-cur.execute(sql)
 
-hans_cates = [c[0] for c in cur.fetchall()]
-cur.execute(sql_hant)
-hant_cates = [c[0] for c in cur.fetchall()]
+def exec_cate():
+    sql = "select * from blog_category"
+    sql_hant = "select * from blog_categoryhant"
+    cur.execute(sql)
 
-for c in hans_cates:
-    if c not in hant_cates:
-        sql = "select * from blog_category where id= %d" % c
-        cur.execute(sql)
-        res = cur.fetchone()
-        insert_ele = []
-        for r in res:
-            if isinstance(r, str):
-                r = fanti_slug = convert_for_mw(r, 'zh-tw')
-            insert_ele.append(r)
-        insert_sql = "insert into blog_categoryhant (id, name, slug, description) values (%d, '%s', '%s', '%s')" % (
-            insert_ele[0], insert_ele[1], insert_ele[2], insert_ele[3]
-        )
-        try:
-            cur.execute(insert_sql)
-            conn.commit()
-            print("成功")
-        except:
-            conn.rollback()
+    hans_cates = [c[0] for c in cur.fetchall()]
+    cur.execute(sql_hant)
+    hant_cates = [c[0] for c in cur.fetchall()]
+
+    for c in hans_cates:
+        if c not in hant_cates:
+            sql = "select * from blog_category where id= %d" % c
+            cur.execute(sql)
+            res = cur.fetchone()
+            insert_ele = []
+            for r in res:
+                if isinstance(r, str):
+                    r = convert_for_mw(r, 'zh-tw')
+                insert_ele.append(r)
+            insert_sql = "insert into blog_categoryhant (id, name, slug, description) values (%d, '%s', '%s', '%s')" % (
+                insert_ele[0], insert_ele[1], insert_ele[2], insert_ele[3]
+            )
+            try:
+                cur.execute(insert_sql)
+                conn.commit()
+                print("cate成功")
+            except:
+                conn.rollback()
+
+
+def exec_tag():
+    sql = "select * from blog_tag"
+    sql_hant = "select * from blog_taghant"
+    cur.execute(sql)
+
+    hans_cates = [c[0] for c in cur.fetchall()]
+    cur.execute(sql_hant)
+    hant_cates = [c[0] for c in cur.fetchall()]
+
+    for c in hans_cates:
+        if c not in hant_cates:
+            sql = "select * from blog_tag where id= %d" % c
+            cur.execute(sql)
+            res = cur.fetchone()
+            insert_ele = []
+            for r in res:
+                if isinstance(r, str):
+                    r = convert_for_mw(r, 'zh-tw')
+                insert_ele.append(r)
+            insert_sql = "insert into blog_taghant (id, name, slug, description) values (%d, '%s', '%s', '%s')" % (
+                insert_ele[0], insert_ele[1], insert_ele[2], insert_ele[3]
+            )
+            try:
+                cur.execute(insert_sql)
+                conn.commit()
+                print("tag成功")
+            except:
+                conn.rollback()
+
+
+def exec_keyword():
+    sql = "select * from blog_keyword"
+    sql_hant = "select * from blog_keywordhant"
+    cur.execute(sql)
+
+    hans_cates = [c[0] for c in cur.fetchall()]
+    cur.execute(sql_hant)
+    hant_cates = [c[0] for c in cur.fetchall()]
+
+    for c in hans_cates:
+        if c not in hant_cates:
+            sql = "select * from blog_keyword where id= %d" % c
+            cur.execute(sql)
+            res = cur.fetchone()
+            insert_ele = []
+            for r in res:
+                if isinstance(r, str):
+                    r = convert_for_mw(r, 'zh-tw')
+                insert_ele.append(r)
+            insert_sql = "insert into blog_keywordhant (id, name) values (%d, '%s')" % (
+                insert_ele[0], insert_ele[1]
+            )
+            try:
+                cur.execute(insert_sql)
+                conn.commit()
+                print("keyword成功")
+            except:
+                conn.rollback()
+
+
+
+def exec_article():
+    sql = "select * from blog_article"
+    sql_hant = "select * from blog_articlehant"
+    cur.execute(sql)
+
+    hans_cates = [c[0] for c in cur.fetchall()]
+    cur.execute(sql_hant)
+    hant_cates = [c[0] for c in cur.fetchall()]
+
+    for c in hans_cates:
+        if c not in hant_cates:
+            sql = "select * from blog_article where id= %d" % c
+            cur.execute(sql)
+            res = cur.fetchone()
+            insert_ele = []
+            for r in res:
+                if isinstance(r, str):
+                    r = convert_for_mw(r, 'zh-tw')
+                insert_ele.append(r)
+            insert_sql = "insert into blog_articlehant (id, title, summary, body, img_link, create_date, update_date, views, slug, author_id, category_id)" \
+                         " values (%d, '%s', '%s', '%s', '%s', '%s', '%s', %d, '%s', %d, %d)" % (
+                insert_ele[0], insert_ele[1], insert_ele[2], insert_ele[3], insert_ele[4], insert_ele[5],
+                insert_ele[6], insert_ele[7], insert_ele[8], insert_ele[9], insert_ele[10]
+            )
+            try:
+                cur.execute(insert_sql)
+                conn.commit()
+                print("article更新成功")
+            except:
+                conn.rollback()
+
+
+if __name__ == '__main__':
+    exec_cate()
+    exec_tag()
+    exec_keyword()
+    exec_article()
