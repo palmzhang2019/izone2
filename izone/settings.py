26c206b6b899b00a2852ffaf9eeb713ce7d85b98
Palm <palm@gmail.com>
2022-02-14 00:21:19 +0800

    修改了163账号

--- izone/settings.py
+++ izone/settings.py
@@ -269,8 +269,8 @@ DATABASES = {
 
 # 邮箱配置
 EMAIL_HOST = 'smtp.163.com'
-EMAIL_HOST_USER = 'palm0318@163.com'
-EMAIL_HOST_PASSWORD = 'LZBCWLGMAOTARKKV'  # 这个不是邮箱密码，而是授权码
+EMAIL_HOST_USER = 'palm2022@163.com'
+EMAIL_HOST_PASSWORD = 'OFKMHZGKFBSNEOCW'  # 这个不是邮箱密码，而是授权码
 EMAIL_PORT = 465  # 由于阿里云的25端口打不开，所以必须使用SSL然后改用465端口
 # 是否使用了SSL 或者TLS，为了用465端口，要使用这个
 EMAIL_USE_SSL = True
