b2ec39670719a8cc05a8f478240b0105bbad8c4e
Palm <palm@gmail.com>
2022-03-19 08:28:13 +0800

    取消了素材

---
+++ izone/database_config-default.py
@@ -0,0 +1,14 @@
+data_config = {
+    'DATAPASS': "daidai",
+    'DATANAME': "www_nihonstudy_n",
+    'DATAUSER': "root",
+    'DATAPORT': 3306,
+    'DATAHOST': "127.0.0.1",
+    "DATANAMEHANT": "nihonstudy_hant"
+}
+
+debug_type = True
+
+ckeditor_upload_path = '/media/upload'
+
+SITE_ID = 4
